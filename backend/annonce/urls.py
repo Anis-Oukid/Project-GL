973a249505c7de599b3461a6a53ead2df1a4271b
Anis OUKID <94572173+Anis-Oukid@users.noreply.github.com>
2023-01-31 06:25:41 +0100

    Update urls.py

--- backend/annonce/urls.py
+++ backend/annonce/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('annonce/<int:pk>/', AnnonceDetail.as_view(), name='annonceDetail'),
-    path('', AnnonceList.as_view(), name='listCreate'),
+    path('annonce/<int:pk>/', AnnonceDetail.as_view(), name='annonce-detail'),
+    path('', AnnonceList.as_view(), name='annonce-list'),
     path('search/', AnnonceSearch.as_view(), name='search'),
     path('add-bookmark', AddToBookmarks.as_view(), name='addBookmark'),
     path('delete-bookmark', RemoveFromBookmarks.as_view(), name='removeBookmark'),
